064d55b19c627c75bce6bd7bc28503ece3197fb9
Oswaldo Lisboa <oswaldolisboa@MacBook-Air-de-Oswaldo.local>
2020-05-03 21:48:52 +0200

    Created some templates.

---
+++ templates/match_template.py
@@ -0,0 +1,63 @@
+template = """<div class="container">
+  <div></div>
+  <div class="inside text">1st Division</div>
+  <div></div>
+  <div class="inside text">Matchday 38</div>
+  <div></div>
+  <div class="inside text">Game 1</div>
+  <div></div>
+  <div></div>
+  <div class="inside"><img class="crest" src="{home_crest}" alt="{home_name}"></div>
+  <div class="inside score">{home_score}</div>
+  <div class="inside score x">x</div>
+  <div class="inside score">{away_score}</div>
+  <div class="inside"><img class="crest" src="{away_crest}" alt="{away_name}"></div>
+  <div></div>
+  <div></div>
+  <div class="inside text">{home_name}</div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div class="inside text">{away_name}</div>
+  <div></div>
+  <div></div>
+  <div class="inside text">{home_twitter}</div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div class="inside text">{away_twitter}</div>
+  <div></div>
+</div>
+
+<style media="screen">
+  .container {
+    background-image: linear-gradient(to right, #1d976c, #63da91);
+    height: 512px;
+    width: 1024px;
+    display: grid;
+    grid-template-columns: 1fr 380px 2fr 140px 2fr 380px 1fr;
+    grid-template-rows: 2fr 380px 1fr 1fr;
+  }
+
+  .inside {
+    margin: auto;
+  }
+
+  .crest {
+    width: 250px;
+    height: auto;
+  }
+
+  .text, .score {
+    color: 	#FFFAFA;
+    font-family: helvetica;
+  }
+
+  .text {
+    font-size: 22;
+  }
+
+  .score {
+    font-size: 64;
+  }
+</style>"""
---
+++ templates/standing_template.py
@@ -0,0 +1,31 @@
+template = """<style>
+    table {
+        color: 	333;
+        font-family: Helvetica;
+        width: 600px;
+        border-collapse:
+        collapse;
+        border-spacing: 0;
+    }
+    td, th {
+        border: 1px solid transparent; /* No more visible border */
+        height: 40px;
+        width: 100px;
+    }
+    th {
+        background: #1d976c; /* Darken header a bit */
+        font-weight: bold;
+        text-align: center;
+        color: 	#FFFAFA;
+    }
+    td {
+        background: #63da91;
+        text-align: center;
+    }
+    table tr:nth-child(odd) td{
+      background-color: #63fa91;
+    }
+    .team {
+      width: 30%;
+    }
+</style>"""
