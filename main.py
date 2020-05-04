c613021692f57463f446b1540ed4be2031ca4fca
Oswaldo Lisboa <oswaldolisboa@MacBook-Air-de-Oswaldo.local>
2020-05-04 22:15:37 +0200

    Created generate_html.py and added to the main.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import pandas as pd
 import json
-from src import create_league, run_league, generate_messages
+from src import create_league, run_league, generate_messages, generate_html
 
 def initialize_league(json_file):
     """
@@ -22,35 +22,41 @@ def generate_schedule_promoted_relegated(teams, fixtures):
     """
 
     """
-    messages = pd.DataFrame(columns = ["TYPE", "DIV", "MD", "GM", "MSG"])
+    messages = pd.DataFrame(columns = ["TYPE", "DIV", "MD", "GM", "MSG", "HTML"])
     promotion = []
     relegation = []
-    for i in range(len(create_league.DIVISIONS.keys())):
+    # for i in range(len(create_league.DIVISIONS.keys())):
+    for i in range(1, 2):
         for j in range(int(fixtures[i].tail(1)["Matchday"])):
             for k in range(len(teams[create_league.DIVISIONS[i+1]].index) // 2):
                 match = fixtures[i].loc[(fixtures[i]["Matchday"] == j + 1) & (fixtures[i]["Match"] == k + 1)]
                 match_msg = generate_messages.generate_match_msg(create_league.DIVISIONS[i+1], teams[create_league.DIVISIONS[i+1]], match, j+1, k+1)
-                messages = messages.append({"TYPE":"1 - Match", "DIV":i+1, "MD":j+1, "GM":k+1,"MSG":match_msg}, ignore_index=True)
+                match_html = generate_html.generate_match_html(create_league.DIVISIONS[i+1], teams[create_league.DIVISIONS[i+1]], match, j+1, k+1)
+                messages = messages.append({"TYPE":"1 - Match", "DIV":i+1, "MD":j+1, "GM":k+1,"MSG":match_msg, "HTML":match_html}, ignore_index=True)
             standings_msg = generate_messages.generate_standings_msg(create_league.DIVISIONS[i+1], j+1)
-            messages = messages.append({"TYPE":"2 - Standings", "DIV":i+1, "MD":j+1, "GM":100,"MSG":standings_msg}, ignore_index=True)
+            standings_html = generate_html.generate_standings_html()
+            messages = messages.append({"TYPE":"2 - Standings", "DIV":i+1, "MD":j+1, "GM":100,"MSG":standings_msg,"HTML":standings_html}, ignore_index=True)
             if (j + 1 != int(fixtures[i].tail(1)["Matchday"])):
                 next_matchday_msg = generate_messages.generate_next_matchday_msg(create_league.DIVISIONS[i+1], j+2)
-                messages = messages.append({"TYPE":"3 - Next Matchday", "DIV":i+1, "MD":j+1, "GM":100,"MSG":next_matchday_msg}, ignore_index=True)
-
+                next_matchday_html = generate_html.generate_next_matchday_html()
+                messages = messages.append({"TYPE":"3 - Next Matchday", "DIV":i+1, "MD":j+1, "GM":100,"MSG":next_matchday_msg,"HTML":next_matchday_html}, ignore_index=True)
         final_standings = run_league.generate_standings(j+1, fixtures[i], teams[create_league.DIVISIONS[i+1]])
         if i + 1 != len(create_league.DIVISIONS.keys()):
             relegated = teams[create_league.DIVISIONS[i+1]].loc[final_standings.tail(3).index]
             relegation.append(relegated)
             relegated_msg = generate_messages.generate_relegated_msg(create_league.DIVISIONS[i+1], create_league.DIVISIONS[i+2], relegated)
-            messages = messages.append({"TYPE":"4 - Relegated", "DIV":i+1, "MD":100, "GM":100,"MSG":relegated_msg}, ignore_index=True)
+            relegated_html = generate_html.generate_relegated_html(create_league.DIVISIONS[i+2], relegated)
+            messages = messages.append({"TYPE":"4 - Relegated", "DIV":i+1, "MD":100, "GM":100,"MSG":relegated_msg,"HTML":relegated_html}, ignore_index=True)
         if i != 0:
             promoted = teams[create_league.DIVISIONS[i+1]].loc[final_standings.head(3).index]
             promotion.append(promoted)
             promoted_msg = generate_messages.generate_promoted_msg(create_league.DIVISIONS[i+1], create_league.DIVISIONS[i], promoted)
-            messages = messages.append({"TYPE":"5 - Promoted", "DIV":i+1, "MD":100, "GM":100,"MSG":promoted_msg}, ignore_index=True)
+            promoted_html = generate_html.generate_promoted_html(create_league.DIVISIONS[i], promoted)
+            messages = messages.append({"TYPE":"5 - Promoted", "DIV":i+1, "MD":100, "GM":100,"MSG":promoted_msg,"HTML":promoted_html}, ignore_index=True)
         champion = teams[create_league.DIVISIONS[i+1]].loc[final_standings.head(1).index[0]]
         champion_msg = generate_messages.generate_champion_msg(create_league.DIVISIONS[i+1], champion)
-        messages = messages.append({"TYPE":"6 - Champions", "DIV":i+1, "MD":100, "GM":100,"MSG":champion_msg}, ignore_index=True)
+        champion_html = generate_html.generate_champion_html(create_league.DIVISIONS[i+1], champion)
+        messages = messages.append({"TYPE":"6 - Champions", "DIV":i+1, "MD":100, "GM":100,"MSG":champion_msg,"HTML":champion_html}, ignore_index=True)
     messages = messages.sort_values(["MD", "GM", "TYPE", "DIV"], ascending=True)
     messages.to_csv("data/messages.csv", index=False)
     return promotion, relegation
@@ -86,5 +92,5 @@ if __name__ == "__main__":
 
     teams, fixtures = initialize_league(json_file)
     promotion, relegation = generate_schedule_promoted_relegated(teams, fixtures)
-    teams = handle_promotion_delegation(teams)
+    # teams = handle_promotion_delegation(teams)
     update_json(teams, json_file)
---
+++ src/generate_html.py
@@ -0,0 +1,87 @@
+from templates import match_template, champion_template, promoted_template, relegated_template, standing_template, next_matchday_template
+
+
+def generate_match_html(division, teams, match, matchday, match_number):
+    """
+
+    """
+    html = match_template.template.format(
+        division=division,
+        matchday=matchday,
+        game=match_number,
+        home_name=teams.loc[match["Home"]]["Name"].iloc[0],
+        home_crest=teams.loc[match["Home"]]["Crest"].iloc[0],
+        home_score=match["HScore"].iloc[0],
+        home_twitter=teams.loc[match["Home"]]["Twitter"].iloc[0],
+        away_score=match["AScore"].iloc[0],
+        away_name=teams.loc[match["Away"]]["Name"].iloc[0],
+        away_crest=teams.loc[match["Away"]]["Crest"].iloc[0],
+        away_twitter=teams.loc[match["Away"]]["Twitter"].iloc[0]
+    )
+    return html
+
+
+def generate_standings_html():
+    """
+
+    """
+    return standing_template.template
+
+
+def generate_next_matchday_html():
+    """
+
+    """
+    retutn next_matchday_template.template
+
+
+def generate_champion_html(division, champion):
+    """
+
+    """
+    html = champion_template.template.format(
+        division=division,
+        champion_name=champion["Name"],
+        champion_crest=champion["Crest"],
+        champion_twitter=champion["Twitter"]
+    )
+
+    return html
+
+
+def generate_promoted_html(promoted_to, promoted_teams):
+    """
+
+    """
+    html = promoted_template.template.format(
+        division=promoted_to,
+        promoted1_crest_file=promoted_teams.iloc[0]["Crest"],
+        promoted2_crest_file=promoted_teams.iloc[1]["Crest"],
+        promoted3_crest_file=promoted_teams.iloc[2]["Crest"],
+        promoted1_name=promoted_teams.iloc[0]["Name"],
+        promoted2_name=promoted_teams.iloc[1]["Name"],
+        promoted3_name=promoted_teams.iloc[2]["Name"],
+        promoted1_twitter=promoted_teams.iloc[0]["Twitter"],
+        promoted2_twitter=promoted_teams.iloc[1]["Twitter"],
+        promoted3_twitter=promoted_teams.iloc[2]["Twitter"],
+    )
+    return html
+
+
+def generate_relegated_html(relegated_to, relegated_teams):
+    """
+
+    """
+    html = relegated_template.template.format(
+        division=relegated_to,
+        relegated1_crest_file=relegated_teams.iloc[0]["Crest"],
+        relegated2_crest_file=relegated_teams.iloc[1]["Crest"],
+        relegated3_crest_file=relegated_teams.iloc[2]["Crest"],
+        relegated1_name=relegated_teams.iloc[0]["Name"],
+        relegated2_name=relegated_teams.iloc[1]["Name"],
+        relegated3_name=relegated_teams.iloc[2]["Name"],
+        relegated1_twitter=relegated_teams.iloc[0]["Twitter"],
+        relegated2_twitter=relegated_teams.iloc[1]["Twitter"],
+        relegated3_twitter=relegated_teams.iloc[2]["Twitter"],
+    )
+    return html
