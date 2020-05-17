fa0ba9ec7abc215c3993b04b70ac2697c89b155d
Oswaldo Lisboa <oswaldolisboa@MacBook-Air-de-Oswaldo.local>
2020-05-17 20:41:36 +0200

    Changed the hashtag with the name of the twitter account.

--- src/generate_messages.py
+++ src/generate_messages.py
@@ -16,7 +16,7 @@ def generate_match_msg(division, teams, match, matchday, match_number):
         match["AScore"].iloc[0],
         teams.loc[match["Away"]]["Twitter"].iloc[0]
     )
-    match_msg += "#{} #{} #{} #LeagueBot".format(
+    match_msg += "#{} #{} #{} #WorldNationsLeagueSimulator".format(
         teams.loc[match["Home"]]["Name"].iloc[0].replace(" ", ""),
         teams.loc[match["Away"]]["Name"].iloc[0].replace(" ", ""),
         division.replace(" ", "")
@@ -27,9 +27,9 @@ def generate_match_msg(division, teams, match, matchday, match_number):
 
 def generate_standings_msg(division, matchday):
     """
-    Generate a string that will be tweeted along with the standing image.
+    Generate a string that will be tweeted along with the standings image.
     """
-    standings_msg = "Standings\n\n{} - Matchday {}\n\n#LeagueBot #{}".format(
+    standings_msg = "Standings\n\n{} - Matchday {}\n\n#WorldNationsLeagueSimulator #{}".format(
         division,
         matchday,
         division.replace(" ", "")
@@ -42,7 +42,7 @@ def generate_next_matchday_msg(division, next_matchday):
     """
     Generate a string that will be tweeted along with the next matchday image.
     """
-    next_matchday_msg = "Next Matchday:\n\n{} - Matchday {}\n\n#LeagueBot #{}".format(
+    next_matchday_msg = "Next Matchday:\n\n{} - Matchday {}\n\n#WorldNationsLeagueSimulator #{}".format(
         division,
         next_matchday,
         division.replace(" ", "")
@@ -53,9 +53,9 @@ def generate_next_matchday_msg(division, next_matchday):
 
 def generate_champion_msg(division, champion):
     """
-
+    Generate a string that will be tweeted along with the champion image of every division.
     """
-    champion_msg = "And the winner of the {} is ...\n\n{}!!!\n\nCongratulations.\n\n#{} #{} #LeagueBot".format(
+    champion_msg = "And the winner of the {} is ...\n\n{}!!!\n\nCongratulations.\n\n#{} #{} #WorldNationsLeagueSimulator".format(
         division,
         champion["Twitter"],
         champion["Name"].replace(" ", ""),
@@ -66,9 +66,9 @@ def generate_champion_msg(division, champion):
 
 def generate_promoted_msg(promoted_from, promoted_to, promoted_teams):
     """
-
+    Generate a string that will be tweeted along with the promoted image of every division.
     """
-    promoted_msg = "The teams promoted to the {} are ...\n\n{}\n{}\n{}\n\nCongratulations!!!\n\n#{} #{} #{} #{} #LeagueBot".format(
+    promoted_msg = "The teams promoted to the {} are ...\n\n{}\n{}\n{}\n\nCongratulations!!!\n\n#{} #{} #{} #{} #WorldNationsLeagueSimulator".format(
         promoted_to,
         promoted_teams.iloc[0]["Twitter"],
         promoted_teams.iloc[1]["Twitter"],
@@ -83,9 +83,9 @@ def generate_promoted_msg(promoted_from, promoted_to, promoted_teams):
 
 def generate_relegated_msg(relegated_from, relegated_to, relegated_teams):
     """
-
+    Generate a string that will be tweeted along with the relegated image of every division.
     """
-    relegated_msg = "The teams relegated to the {} are ...\n\n{}\n{}\n{}\n\nBetter luck next time.\n\n#{} #{} #{} #{} #LeagueBot".format(
+    relegated_msg = "The teams relegated to the {} are ...\n\n{}\n{}\n{}\n\nBetter luck next time.\n\n#{} #{} #{} #{} #WorldNationsLeagueSimulator".format(
         relegated_to,
         relegated_teams.iloc[0]["Twitter"],
         relegated_teams.iloc[1]["Twitter"],
